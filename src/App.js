b29f63230128668714ba0ca495faa9814c915bfb
Sufian20 <jalalisufian300@gmail.com>
2021-08-27 05:44:13 -0700

    commit first

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+function App() {
+  return (
+    <div className="app">
+      <h1>I acpect the amazon clone chalenge</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/conponents/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBym3TVY7uTvCRpz2aswoCPkZtY0gOHL5A",
+    authDomain: "clone-70932.firebaseapp.com",
+    projectId: "clone-70932",
+    storageBucket: "clone-70932.appspot.com",
+    messagingSenderId: "16219945429",
+    appId: "1:16219945429:web:bb4d544478c6b39914e55c"
+  };
+
+  export default  firebaseConfig
